237ee89064b8efab3aaa5153c0a3fcd9cf2271a0
Richard Seal <richard.seal@perficient.com>
2019-08-12 13:48:29 -0400

    Clean up of Helix stds

--- src/Foundation/SecurityHeaders/tests/LocalAddin.cs
+++
@@ -1,9 +0,0 @@
-using NUnit.Core.Extensibility;
-
-namespace Northwoods.Foundation.SecurityHeaders.Tests
-{
-    [NUnitAddin]
-    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
-    {   
-    }
-}
